31ebc49ee6023e4ffe0172406ef820ac05873757
MrCoderI <26aidenz@wab.edu>
2021-04-27 09:59:28 +0800

    V0.2.1 - Better Enemy Generation, Different Start Screen & Score System

--- Test_Game_6/src/Main.cpp
+++ Test_Game_6/src/Main.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 const float pi = 3.1415;
@@ -11,11 +12,16 @@ const int h = 1500, w = 1500;
 int main() {
     sf::RenderWindow window(sf::VideoMode(h, w), "My window");
     // run the main loop
-    bool mouseButton = false, gameEnd = false;
+    bool mouseButton = false, gameEnd = true;
     float speed = 1000, eSpeed = 100, xV = 0, yV = 0, deltaTime = 0, enemyTime = 0, enemyAmn = 0, enemyTimeM = 0.5;
-    int hitbox = 100, hitboxE = 100;
+    int hitbox = 100, hitboxE = 100, wordSize = 100;
+    string scoreString = "0";
     vector<float> xVs, yVs;
     vector<sf::Sprite> enemies;
+    sf::Clock pointUpdateC;
+    sf::Time pointUpdateT;
+    sf::Clock pointScoreC;
+    sf::Time pointScoreT;
     sf::Clock enemyTimeC;
     sf::Time enemyTimeT;
     sf::Clock deltaTimeC;
@@ -25,13 +31,21 @@ int main() {
     sf::Texture enemyT;
     enemyT.loadFromFile("resources/Enemy.png");
     sf::Texture endScreenT;
-    endScreenT.loadFromFile("resources/Restart Screen.png");
+    endScreenT.loadFromFile("resources/StartScreen.png");
     sf::Sprite player;
     player.setTexture(playerT);
     player.setOrigin(hitbox/2,hitbox/2);
     player.setPosition(h/2,w/2);
     sf::Sprite endScreen;
     endScreen.setTexture(endScreenT);
+    sf::Font scoreFont;
+    scoreFont.loadFromFile("resources/sansation.ttf");
+    sf::Text score;
+    score.setFont(scoreFont);
+    score.setString(scoreString);
+    score.setCharacterSize(wordSize);
+    score.setFillColor(sf::Color::Black);
+    score.setPosition(1500.0-2.0*(wordSize*scoreString.size())/3.0,-20);
     while (window.isOpen()) {
         if(gameEnd == false) {
             sf::Event event;
@@ -47,18 +61,41 @@ int main() {
                     mouseButton = false;
                 }
             }
+            pointUpdateT = pointUpdateC.getElapsedTime();
+            if(pointUpdateT.asSeconds() >= 0.5) {
+                pointUpdateC.restart();
+                pointScoreT = pointScoreC.getElapsedTime();
+                int pointScore = pointScoreT.asSeconds();
+                stringstream conv;
+                conv << pointScore;
+                scoreString = conv.str();
+                score.setString(scoreString);
+                score.setPosition(1500.0-2.0*(wordSize*scoreString.size())/3.0,-20);
+            }
             enemyTimeT = enemyTimeC.getElapsedTime();
             enemyTime = enemyTimeT.asSeconds();
             if(enemyTime >= enemyTimeM) {
                 enemyTimeM*=0.995;
                 eSpeed*=1.005;
                 enemyTimeC.restart();
-                float angle = (rand()%180);
+                int type = rand()%4;
+                float angle = (rand()%180)+type*90;
                 sf::Sprite tmp;
                 tmp.setTexture(enemyT);
                 tmp.setOrigin(hitboxE/2,hitboxE/2);
                 tmp.setRotation(angle+90);
-                tmp.setPosition(rand()%1500,0);
+                if(type == 0) {
+                    tmp.setPosition(rand()%1500,0);
+                }
+                if(type == 1) {
+                    tmp.setPosition(1500,rand()%1500);
+                }
+                if(type == 2) {
+                    tmp.setPosition(rand()%1500,1500);
+                }
+                if(type == 3) {
+                    tmp.setPosition(0,rand()%1500);
+                }
                 xVs.push_back(cos(angle/180*pi)*eSpeed);
                 yVs.push_back(sin(angle/180*pi)*eSpeed);
                 enemies.push_back(tmp);
@@ -70,6 +107,11 @@ int main() {
             deltaTimeC.restart();
             //player AI
             player.move(xV*deltaTime,yV*deltaTime);
+            float pX = player.getPosition().x;
+            float pY = player.getPosition().y;
+            if(pX > 1500 || pX < 0 || pY > 1500 || pY < 0) {
+                gameEnd = true;
+            }
             //enemy AI
             for(int i = 0; i < enemyAmn; i++) {
                 enemies[i].move(sf::Vector2f(xVs[i]*deltaTime,yVs[i]*deltaTime));
@@ -110,17 +152,12 @@ int main() {
             }
             xV=xV*0.999;
             yV=yV*0.999;
-            if(abs(xV) <= deltaTime) {
-                xV=0;
-            }
-            if(abs(yV) <= deltaTime) {
-                yV=0;
-            }
             window.clear(sf::Color(192,192,192));
             window.draw(player);
             for(int i = 0; i < enemyAmn; i++) {
                 window.draw(enemies[i]);
             }
+            window.draw(score);
             window.display();
         }
         if(gameEnd == true) {
@@ -139,9 +176,12 @@ int main() {
                     }
                     enemyTimeC.restart();
                     deltaTimeC.restart();
+                    pointUpdateC.restart();
+                    pointScoreC.restart();
+                    scoreString = "0";
                     player.setPosition(h/2,w/2);
                     mouseButton = false;
-                    speed = 1000, eSpeed = 100, xV = 0, yV = 0, deltaTime = 0, enemyTime = 0, enemyAmn = 0, enemyTimeM = 2.0;
+                    speed = 1000, eSpeed = 100, xV = 0, yV = 0, deltaTime = 0, enemyTime = 0, enemyAmn = 0, enemyTimeM = 0.5;
                     hitbox = 100, hitboxE = 100;
                     gameEnd = false;
                     break;
@@ -149,6 +189,7 @@ int main() {
             }
             window.clear(sf::Color(192,192,192));
             window.draw(endScreen);
+            window.draw(score);
             window.display();
         }
     }
