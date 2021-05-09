bb5e9e56a430bd854ba208b1a2c7d0d88a36e33d
Feng Zhou <fengzhou@gmail.com>
2021-05-09 12:43:51 +0800

    Reset savefile. Add app icon.

--- Test_Game_6/ship.sh
+++ Test_Game_6/ship.sh
@@ -2,7 +2,11 @@ if ! codesign --force --deep --sign "Apple Development: fengzhou@gmail.com (J9GF
 	echo "codesign failed"
 	exit 1
 fi
+if ! codesign --force --deep --sign "Apple Development: fengzhou@gmail.com (J9GF8BDE5Y)" build/Triangle_Dodge.dmg; then
+	echo "codesign failed"
+	exit 1
+fi
 cd build
-zip -r Triangle_Dodge.zip Triangle_Dodge.app
-scp Triangle_Dodge.zip 192.168.1.200:/var/www/html/tmp
+ditto -c -k --sequesterRsrc --keepParent Triangle_Dodge.app Triangle_Dodge.zip
+scp Triangle_Dodge.zip Triangle_Dodge.dmg 192.168.1.200:/var/www/html/tmp
 
