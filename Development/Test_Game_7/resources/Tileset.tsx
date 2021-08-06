5ae1873064610b2019929bf585fd855b3cbb09fd
MrCoderI <60565487+MrCoderI@users.noreply.github.com>
2021-08-06 16:53:31 +0800

    0.8.2 - Level 4!

---
+++ Development/Test_Game_7/resources/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Tileset" tilewidth="200" tileheight="200" tilecount="33" columns="11">
+ <image source="Tileset.png" width="2200" height="600"/>
+</tileset>
